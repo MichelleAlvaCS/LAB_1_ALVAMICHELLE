5366bba09abbc3962d239e7c7573676e08f75930
Alva <rdlupine@yahoo.com>
2020-09-03 13:04:33 -0700

    Set up template with two structures

--- LAB_1_ALVAMICHELLE/LAB_1_ALVAMICHELLE.cpp
+++ LAB_1_ALVAMICHELLE/LAB_1_ALVAMICHELLE.cpp
@@ -4,6 +4,24 @@
 #include <string>
 using namespace std;
 
+//structure
+struct InventoryItem {
+	string itemId;
+	string itemName;
+	int itemQuantity;
+	double itemPrice;
+
+};
+
+//dummy structure
+struct tempStruct {
+	string itemId;
+	string itemName;
+	int itemQuantity;
+	double itemPrice;
+
+};
+
 int main() {
 
 
